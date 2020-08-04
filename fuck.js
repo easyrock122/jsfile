1433d1e28f5449f6c0c3214c388f4f1f61e06716
easyrock122 <easyrack2@gmail.com>
2020-08-04 19:33:37 +0900

    dsdeee

--- fuck.js
+++ fuck.js
@@ -2,12 +2,16 @@ let b = document.getElementById("bar");
 
 b.addEventListener("keyup",()=> {
     if(event.keyCode ===13){
-        if (100 >= b.value && b.value >= 61 ) {
+        if (100 >= b.value && b.value >= 81 ) {
+            alert("오늘의 기분은 매우 좋음");
+           } else if (81 > b.value && b.value >= 61) {
             alert("오늘의 기분은 좋음");
-           } else if (61 > b.value && b.value >= 30) {
+            } else if(61 > b.value && b.value >= 41){
             alert("오늘의 기분은 보통");
-            } else {
-            alert("오늘의 기분은 나쁨");
+            }else if(41 > b.value && b.value >= 21){
+                alert("오늘의 기분은 나쁨");
+            }else {
+                alert("오늘의 기분은 메우 나쁨");
         }
     }
 }
