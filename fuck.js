bf93f5c2baee531d2587ffca26d4597f80c44e28
easyrock122 <easyrack2@gmail.com>
2020-08-04 18:44:09 +0900

    dkdkdsss

--- fuck.js
+++ fuck.js
@@ -1,7 +1,14 @@
-let b = document.getElementById("bar").vlaue;
+let b = document.getElementById("bar");
 
 b.addEventListener("keyup",()=> {
     if(event.keyCode ===13){
-     alert(b)
+        if (100 >= b.value && b.value >= 61 ) {
+            alert("오늘의 기분은 좋음");
+           } else if (61 > b.value && b.value >= 30) {
+            alert("오늘의 기분은 보통");
+            } else {
+            alert("오늘의 기분은 나쁨");
+        }
     }
-    });
+}
+) 
